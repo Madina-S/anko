a742951c5be3187e8a35c9efe08c6f5620ecc676
Madina-S <m.saidova@student.inha.uz>
2019-05-23 11:38:56 +0500

    Update layout.kt

--- anko/idea-plugin/xml-converter/testData/linearLayout/layout.kt
+++ anko/idea-plugin/xml-converter/testData/linearLayout/layout.kt
@@ -3,9 +3,9 @@ linearLayout {
         gravity = Gravity.END
         bottomMargin = dip(5)
     }
-    textView.lparams(width = wrapContent, height = matchParent) {
+    textView{}.lparams(width = wrapContent, height = matchParent) {
         gravity = Gravity.CENTER
-        weight = 5
+        weight = 5F
         margin = dip(10)
     }
 }
